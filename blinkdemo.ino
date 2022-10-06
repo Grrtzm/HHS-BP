ee16af3a6d6699a9711c78237e275c18ba6bc8d6
Grrtzm <gneijsel@gmail.com>
2022-10-06 22:15:57 +0200

    Create blinkdemo.ino
    To test installation of Arduino IDE for use with Microbit V2

---
+++ blinkdemo.ino
@@ -0,0 +1,24 @@
+const int COL1 = 4;     // Column #1 control
+const int LED = 21;     // 'row 1' led
+ 
+void setup() {  
+  Serial.begin(9600);
+  
+  Serial.println("Micro:bit V2 is ready!");
+ 
+  // because the LEDs are multiplexed, 
+  // we must ground the opposite side of the LED
+  pinMode(COL1, OUTPUT);
+  digitalWrite(COL1, LOW); 
+   
+  pinMode(LED, OUTPUT);   
+}
+ 
+void loop(){
+  Serial.println("blink!");
+  
+  digitalWrite(LED, HIGH);
+  delay(500);
+  digitalWrite(LED, LOW);
+  delay(500);
+}
