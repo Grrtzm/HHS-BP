bf5a9dea4b5c29cc24d06ded7315a2ac8cbbf3b0
Grrtzm <gneijsel@gmail.com>
2022-10-09 20:36:13 +0200

    Update blinkdemo.ino
    Microbit V2

--- blinkdemo.ino
+++ blinkdemo.ino
@@ -1,24 +1,23 @@
-const int COL1 = 4;     // Column #1 control
-const int LED = 21;     // 'row 1' led
- 
-void setup() {  
+// Led matrix pinnen voor Microbit V2
+const int COL1 = 4;   // Column #1 control // pin 3  bij Microbit V1
+const int ROW1 = 21;  // 'row 1' led       // pin 26 bij Microbit V1
+
+void setup() {
   Serial.begin(9600);
-  
-  Serial.println("Micro:bit V2 is ready!");
- 
-  // because the LEDs are multiplexed, 
-  // we must ground the opposite side of the LED
-  pinMode(COL1, OUTPUT);
-  digitalWrite(COL1, LOW); 
-   
-  pinMode(LED, OUTPUT);   
+  Serial.println("Microbit V2 is ready!");
+
+  // Omdat de LEDs in een matrix staan moet zowel de plus als de min aangestuurd worden.
+  // De stroom loopt immers van + naar -
+  pinMode(COL1, OUTPUT);  // kolom is de -
+  pinMode(ROW1, OUTPUT);  // rij is de +
+  digitalWrite(COL1, LOW);
 }
- 
-void loop(){
+
+void loop() {
   Serial.println("blink!");
-  
-  digitalWrite(LED, HIGH);
+
+  digitalWrite(ROW1, HIGH);
   delay(500);
-  digitalWrite(LED, LOW);
+  digitalWrite(ROW1, LOW);
   delay(500);
 }
